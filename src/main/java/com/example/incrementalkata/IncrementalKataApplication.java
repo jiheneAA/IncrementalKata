0c03296f38bc7119afcf4f8b25890fd8da080f2a
jiheneAA <jihene.abdelhedi1@gmail.com>
2023-02-18 23:28:21 +0100

    Create Java 17 project

---
+++ src/main/java/com/example/incrementalkata/IncrementalKataApplication.java
@@ -0,0 +1,13 @@
+package com.example.incrementalkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncrementalKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncrementalKataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/incrementalkata/IncrementalKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.incrementalkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncrementalKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
